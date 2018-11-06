7e3ea9b634cf50f20ea511cd55d56595c9fceeee
Simon Lindroos <simj91@gmail.com>
2018-11-06 11:29:11 +0100

    Add cloudwatch action for get_metric_statistic_value
    Signed-off-by: Simon Lindroos <simj91@gmail.com>

--- chaosaws/cloudwatch/probes.py
+++ chaosaws/cloudwatch/probes.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+
 from chaoslib.exceptions import FailedActivity
 from chaoslib.types import Configuration, Secrets
+from logzero import logger
 
 from chaosaws import aws_client
 
-__all__ = ["get_alarm_state_value"]
+__all__ = ["get_alarm_state_value", "get_metric_statistic_value"]
 
 
 def get_alarm_state_value(alarm_name: str,
@@ -23,3 +26,62 @@ def get_alarm_state_value(alarm_name: str,
             "CloudWatch alarm name {} not found".format(alarm_name)
         )
     return response["MetricAlarms"][0]["StateValue"]
+
+
+def get_metric_statistic_value(namespace: str,
+                               metric_name: str,
+                               dimension_name: str,
+                               dimension_value: str,
+                               period: str,
+                               statistic: str = None,
+                               extended_statistic: str = None,
+                               unit: str = None,
+                               configuration: Configuration = None,
+                               secrets: Secrets = None):
+    client = aws_client("cloudwatch", configuration, secrets)
+
+    if statistic is None and extended_statistic is None:
+        raise FailedActivity(
+            'You must supply argument for statistic or extended_statistic'
+        )
+
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(seconds=period)
+    request_kwargs = {
+        'Namespace': namespace,
+        'MetricName': metric_name,
+        'Dimensions': [
+            {
+                'Name': dimension_name,
+                'Value': dimension_value
+            }
+        ],
+        'StartTime': start_time,
+        'EndTime': end_time,
+        'Period': period
+    }
+
+    if statistic is not None:
+        request_kwargs['Statistics'] = [statistic]
+    if extended_statistic is not None:
+        request_kwargs['ExtendedStatistics'] = [extended_statistic]
+    if unit is not None:
+        request_kwargs['Unit'] = unit
+
+    logger.debug('Request arguments: {}'.format(request_kwargs))
+    response = client.get_metric_statistics(**request_kwargs)
+
+    datapoints = response['Datapoints']
+    if len(datapoints) == 0:
+        raise FailedActivity(
+            'No datapoints found for metric {}.{}.{}.{}'.format(
+                namespace, metric_name, dimension_name, dimension_value
+            )
+        )
+
+    datapoint = datapoints[0]
+    logger.debug('Response: {}'.format(response))
+    if statistic is not None:
+        return datapoint[statistic]
+    elif extended_statistic is not None:
+        return datapoint['ExtendedStatistics'][extended_statistic]
--- tests/cloudwatch/test_cloudwatch_probes.py
+++ tests/cloudwatch/test_cloudwatch_probes.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timezone
 from unittest.mock import MagicMock, patch
-from chaosaws.cloudwatch.probes import get_alarm_state_value
-from chaoslib.exceptions import FailedActivity
+
 import pytest
+from chaoslib.exceptions import FailedActivity
+
+from chaosaws.cloudwatch.probes import (get_alarm_state_value,
+                                        get_metric_statistic_value)
 
 
 @patch('chaosaws.cloudwatch.probes.aws_client', autospec=True)
@@ -34,3 +38,138 @@ def test_cloudwatch_get_alarm_state_value_not_found(aws_client):
     with pytest.raises(FailedActivity):
         get_alarm_state_value(alarm_name)
     client.describe_alarms.assert_called_with(AlarmNames=[alarm_name])
+
+
+@patch('chaosaws.cloudwatch.probes.datetime', autospec=True)
+@patch('chaosaws.cloudwatch.probes.aws_client', autospec=True)
+def test_cloudwatch_get_metric_statistic_value_statistic_ok(aws_client,
+                                                            mock_datetime):
+    client = MagicMock()
+    aws_client.return_value = client
+    mock_datetime.utcnow.return_value = datetime(2015, 1, 1, 15, 15,
+                                                 tzinfo=timezone.utc)
+
+    namespace = 'AWS/Lambda'
+    metric_name = 'Invocations'
+    dimension_name = 'FunctionName'
+    dimension_value = 'MyFunction'
+    period = 120
+    statistic = 'Sum'
+    extended_statistic = None
+    unit = 'Count'
+    client.get_metric_statistics.return_value = {
+        'Datapoints': [{statistic: '4753.0'}]
+    }
+
+    result = get_metric_statistic_value(
+        namespace=namespace,
+        metric_name=metric_name,
+        dimension_name=dimension_name,
+        dimension_value=dimension_value,
+        period=period,
+        statistic=statistic,
+        extended_statistic=extended_statistic,
+        unit=unit
+    )
+
+    assert result == '4753.0'
+    client.get_metric_statistics.assert_called_with(
+        Namespace=namespace,
+        MetricName=metric_name,
+        Dimensions=[{'Name': dimension_name, 'Value': dimension_value}],
+        Period=period,
+        StartTime=datetime(2015, 1, 1, 15, 13, tzinfo=timezone.utc),
+        EndTime=datetime(2015, 1, 1, 15, 15, tzinfo=timezone.utc),
+        Statistics=[statistic],
+        Unit=unit
+    )
+
+
+@patch('chaosaws.cloudwatch.probes.datetime', autospec=True)
+@patch('chaosaws.cloudwatch.probes.aws_client', autospec=True)
+def test_cloudwatch_get_metric_statistic_value_extended_statistic_ok(
+        aws_client,
+        mock_datetime):
+    client = MagicMock()
+    aws_client.return_value = client
+    mock_datetime.utcnow.return_value = datetime(2015, 1, 1, 15, 15,
+                                                 tzinfo=timezone.utc)
+
+    namespace = 'AWS/Lambda'
+    metric_name = 'Invocations'
+    dimension_name = 'FunctionName'
+    dimension_value = 'MyFunction'
+    period = 120
+    statistic = None
+    extended_statistic = 'p99'
+    client.get_metric_statistics.return_value = {
+        'Datapoints': [{'ExtendedStatistics': {extended_statistic: '4753.0'}}]
+    }
+    unit = None
+
+    result = get_metric_statistic_value(
+        namespace=namespace,
+        metric_name=metric_name,
+        dimension_name=dimension_name,
+        dimension_value=dimension_value,
+        period=period,
+        statistic=statistic,
+        extended_statistic=extended_statistic,
+        unit=unit
+    )
+
+    assert result == '4753.0'
+    client.get_metric_statistics.assert_called_with(
+        Namespace=namespace,
+        MetricName=metric_name,
+        Dimensions=[{'Name': dimension_name, 'Value': dimension_value}],
+        Period=period,
+        StartTime=datetime(2015, 1, 1, 15, 13, tzinfo=timezone.utc),
+        EndTime=datetime(2015, 1, 1, 15, 15, tzinfo=timezone.utc),
+        ExtendedStatistics=[extended_statistic],
+    )
+
+
+@patch('chaosaws.cloudwatch.probes.datetime', autospec=True)
+@patch('chaosaws.cloudwatch.probes.aws_client', autospec=True)
+def test_cloudwatch_get_metric_statistic_value_no_datapoints(
+        aws_client, mock_datetime):
+    client = MagicMock()
+    aws_client.return_value = client
+    mock_datetime.utcnow.return_value = datetime(2015, 1, 1, 15, 15,
+                                                 tzinfo=timezone.utc)
+
+    namespace = 'AWS/Lambda'
+    metric_name = 'Invocations'
+    dimension_name = 'FunctionName'
+    dimension_value = 'MyFunction'
+    period = 120
+    statistic = 'Sum'
+    extended_statistic = None
+    unit = 'Count'
+    client.get_metric_statistics.return_value = {
+        'Datapoints': []
+    }
+
+    with pytest.raises(FailedActivity):
+        get_metric_statistic_value(
+            namespace=namespace,
+            metric_name=metric_name,
+            dimension_name=dimension_name,
+            dimension_value=dimension_value,
+            period=period,
+            statistic=statistic,
+            extended_statistic=extended_statistic,
+            unit=unit
+        )
+
+    client.get_metric_statistics.assert_called_with(
+        Namespace=namespace,
+        MetricName=metric_name,
+        Dimensions=[{'Name': dimension_name, 'Value': dimension_value}],
+        Period=period,
+        StartTime=datetime(2015, 1, 1, 15, 13, tzinfo=timezone.utc),
+        EndTime=datetime(2015, 1, 1, 15, 15, tzinfo=timezone.utc),
+        Statistics=[statistic],
+        Unit=unit
+    )
